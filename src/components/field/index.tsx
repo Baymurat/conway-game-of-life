4b61b0652ecee1acaf4fd53da7662852504dff5a
Baymurat <baymurat.orazov@mail.ru>
2023-03-24 15:56:00 +0500

    feat: add painted gameboard

--- src/components/field/index.tsx
+++ src/components/field/index.tsx
@@ -1,15 +1,74 @@
+import { useEffect, useState } from 'react'
 import { Box } from '@mui/material'
+import { useCustomRef } from '../../utils/hooks';
+import { useControlContext } from '../../context/controlContext'
 
-type Props = {}
+const GameField = () => {
+  const { gridWidth, gridHeight, isGameStarted } = useControlContext();
+  const [divRef, setDivRef] = useCustomRef<HTMLDivElement>();
+  const [basis, setBasis] = useState<number>(0)
+  const [cells, setCells] = useState<number[][]>([[]])
 
-const GameField = (props: Props) => {
+  useEffect(() => {
+    if (divRef !== null) {
+      const { clientWidth, clientHeight } = divRef
+      const basisValue = gridWidth > gridHeight
+        ? clientWidth / gridWidth
+        : clientHeight / gridHeight
+
+      setBasis(basisValue)
+    }
+
+  }, [divRef, gridWidth, gridHeight])
+
+  useEffect(() => {
+    const arr = new Array(gridHeight).fill(0).map(() => new Array(gridWidth).fill(0))
+    setCells(arr)
+  }, [gridWidth, gridHeight])
+
   return (
     <Box
       flex="10 0 auto"
       bgcolor="#fff"
       height="100%"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      justifyContent="center"
+      ref={setDivRef}
     >
-      GameField
+      {cells.map((row, y) => (
+        <Box
+          display="flex"
+        >
+          {row.map((cell, x) => (
+            <Box
+              border="1px solid gray"
+              width={basis}
+              height={basis}
+              bgcolor={`${cell === 1 ? '#000' : '#fff'}`}
+              sx={{
+                '&:hover': {
+                  cursor: `${isGameStarted ? 'initial' : 'pointer'}`
+                }
+              }}
+              onClick={() => {
+                if (!isGameStarted) {
+                  setCells((prev) => prev.map((row, yU) => row.map((cell, xU) => {
+                    if (y === yU && x === xU) {
+                      return cell === 1 ? 0 : 1
+                    }
+
+                    return cell
+                  })))
+                }
+              }}
+            >
+
+            </Box>
+          ))}
+        </Box>
+      ))}
     </Box>
   )
 }
