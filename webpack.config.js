0d7a6432f1d25ffeec2c3c7b11fc524ba71af206
Baymurat <baymurat.orazov@mail.ru>
2023-03-24 12:08:28 +0500

    init: initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    mian: './src/index.tsx'
+  },
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist')
+  },
+  devtool: 'source-map',
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+    hot: true,
+    liveReload: true
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: 'ts-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html')
+    })
+  ]
+}
